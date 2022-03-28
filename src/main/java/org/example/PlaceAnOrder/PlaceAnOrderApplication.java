0628f77f6662323f0ac78c237dbec36fbb75042e
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-03-28 11:36:55 -0500

    Initialize project

---
+++ src/main/java/org/example/PlaceAnOrder/PlaceAnOrderApplication.java
@@ -0,0 +1,13 @@
+package org.example.PlaceAnOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceAnOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceAnOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/PlaceAnOrder/PlaceAnOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.PlaceAnOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceAnOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
