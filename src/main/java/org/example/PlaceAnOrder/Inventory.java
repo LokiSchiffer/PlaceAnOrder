b98dc71dadd56caa3de302d2abc90c7b64af5395
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-03-29 22:00:19 -0500

    Set internal methods for templates

--- src/main/java/org/example/PlaceAnOrder/Inventory.java
+++ src/main/java/org/example/PlaceAnOrder/Inventory.java
@@ -2,13 +2,28 @@ package org.example.PlaceAnOrder;
 
 public class Inventory {
 
-    private int productCode;
+    private final int productCode;
     private int productQuantity;
-    private String productName;
+    private final String productName;
 
     public Inventory(int productCode, int productQuantity, String productName) {
         this.productCode = productCode;
         this.productQuantity = productQuantity;
         this.productName = productName;
+    }
+
+    public int getProductCode() {
+        return productCode;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        if (productQuantity <= 0)
+            System.out.println("Quantity not accepted, it should" +
+                    " be greater than 0");
+        this.productQuantity -= productQuantity;
     }
 }
--- src/main/java/org/example/PlaceAnOrder/User.java
+++ src/main/java/org/example/PlaceAnOrder/User.java
@@ -2,14 +2,37 @@ package org.example.PlaceAnOrder;
 
 public class User {
 
-    private int creditCardNumber;
-    private double availableFunds;
+    private final int creditCardNumber;
+    private double totalFunds;
     private double reservedFunds;
-    private String username;
+    private final String username;
 
-    public User(int creditCardNumber, double availableFunds, String username) {
+    public User(int creditCardNumber, double totalFunds, String username) {
         this.creditCardNumber = creditCardNumber;
-        this.availableFunds = availableFunds;
+        this.totalFunds = totalFunds;
         this.username = username;
+    }
+
+    public int getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public double getAvailableFunds() {
+        return totalFunds-reservedFunds;
+    }
+
+    public void setReservedFunds(double reservedFunds) {
+        if (reservedFunds <= 0)
+            System.out.println("Value not accepted, it should" +
+                    " be greater than 0");
+        else if (reservedFunds < totalFunds)
+            this.reservedFunds = reservedFunds;
+        else
+            System.out.println("Not enough funds");
+    }
+
+    public void debit() {
+        this.totalFunds -= reservedFunds;
+        this.reservedFunds = 0;
     }
 }
